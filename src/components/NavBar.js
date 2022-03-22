62cfa57395be4392fb45ff5dfe8344bc0e14887b
Elias Acosta <eliasvalentinacosta2000@gmail.com>
2022-03-22 18:54:16 -0300

    NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,6 @@ const NavBar = () => {
     <nav>
       <h2>TECNOSHOP</h2>
       <button>PRODUCTOS</button>
-      <a>
-        <li>Placas de video</li>
-      </a>
-      <li>Procesadores</li>
-      <li>Motherboards</li>
       <button>CONTACTO</button>
       <button>OFERTAS</button>
     </nav>
