a402ab3dd9c2a12ff3452eb5bbe590b7d260e1c3
Elias Acosta <eliasvalentinacosta2000@gmail.com>
2022-03-30 17:20:38 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -14,18 +14,6 @@ function App() {
       <NavBar />
       <ItemListContainer greeting="Bienvenido a TECNOSHOP" />
       <ItemCount initial={1} stock={25} onAdd={onAdd} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TECNOSHOP
-        </a>
-      </header>
     </div>
   );
 }
