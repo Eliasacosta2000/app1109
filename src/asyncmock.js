9a33c982e7a3a0d542e66ba18a708e6d9649dcfe
Elias Acosta <eliasvalentinacosta2000@gmail.com>
2022-03-30 17:56:38 -0300

    cambios productos

---
+++ src/asyncmock.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: 1,
+    name: "Ryzen 5 3600",
+    price: 42000,
+    category: "Procesador",
+    stock: 30,
+    description: "descripcion del Ryzen 5 3600",
+  },
+  {
+    id: 2,
+    name: "Motherboard Gigabyte A320M-H AM4",
+    price: 9000,
+    category: "Motherboard",
+    stock: 50,
+    description: "descripcion de la motherboard",
+  },
+  {
+    id: 3,
+    name: "Memoria Ram Adata Xpg Gammix D20 8gb 3200 Mhz Ddr4 Black",
+    price: 6000,
+    category: "Memoria Ram",
+    stock: 45,
+    description: "descripcion de la memoria ram",
+  },
+];
+
+export const getProducts = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(products);
+    }, 2000);
+  });
+};
--- src/components/ItemListConteiner.js
+++ src/components/ItemListConteiner.js
@@ -1,5 +1,30 @@
+import React from "react";
+import { getProducts } from "../../asyncmock";
+import { useState, useEffect } from "react";
+import ItemList from "./components/ItemList";
+
 const ItemListContainer = ({ greeting }) => {
-  return <h1>{greeting}</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((response) => {
+      setProducts(response);
+    });
+  }, []);
+
+  console.log(products);
+
+  return (
+    <div>
+      <h1>{greeting}</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
+      <ItemList products={products} />
+    </div>
+  );
 };
 
 export default ItemListContainer;
