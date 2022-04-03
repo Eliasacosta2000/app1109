d9038557476fe295d4d1b8af3b7ee23a97a099c3
Elias Acosta <eliasvalentinacosta2000@gmail.com>
2022-04-03 15:54:58 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListConteiner";
 import ItemCount from "./components/ItemCount";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 
 function App() {
   const onAdd = (quantity) => {
@@ -11,9 +13,17 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar />
-      <ItemListContainer greeting="Bienvenido a TECNOSHOP" />
-      <ItemCount initial={1} stock={25} onAdd={onAdd} />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:categoryId" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+        </Routes>
+        <ItemListContainer greeting="Bienvenido a TECNOSHOP" />
+        <ItemCount initial={1} stock={25} onAdd={onAdd} />
+      </BrowserRouter>
+      <p>2022 C</p>
     </div>
   );
 }
